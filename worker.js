e070efef338942b15e3569ba7c120ef6ae9fd58c
Andrea Fletcher <50600470+BitwiseAndrea@users.noreply.github.com>
2025-07-31 20:45:42 -0700

    okeeeeee

--- worker.js
+++ worker.js
@@ -1,41 +1,51 @@
-import indexPageHtml from './index.html';
-import testsPageHtml from './tests.html';
-import wordleBotJs from './wordle-bot.js';
-import mainJs from './main.js';
-import stylesCs from './styles.css';
+// Simple proxy to serve files from GitHub
+const GITHUB_BASE = 'https://raw.githubusercontent.com/BitwiseAndrea/wordle/refs/heads/main/';
 
 export default {
   async fetch(request) {
     const url = new URL(request.url);
     const path = url.pathname;
 
-    // Handle different routes
+    // Handle different routes by proxying to GitHub
     switch (path) {
       case '/':
       case '/index.html':
-        return new Response(indexPageHtml, {
+        const indexResponse = await fetch(GITHUB_BASE + 'index.html');
+        const indexHtml = await indexResponse.text();
+        return new Response(indexHtml, {
           headers: { 'Content-Type': 'text/html;charset=UTF-8' }
         });
       
       case '/tests':
       case '/tests.html':
-        return new Response(testsPageHtml, {
+        const testsResponse = await fetch(GITHUB_BASE + 'tests.html');
+        const testsHtml = await testsResponse.text();
+        return new Response(testsHtml, {
           headers: { 'Content-Type': 'text/html;charset=UTF-8' }
         });
       
       case '/wordle-bot.js':
-        return new Response(wordleBotJs, {
+        const botResponse = await fetch(GITHUB_BASE + 'wordle-bot.js');
+        return new Response(await botResponse.text(), {
           headers: { 'Content-Type': 'application/javascript;charset=UTF-8' }
         });
       
       case '/main.js':
-        return new Response(mainJs, {
+        const mainResponse = await fetch(GITHUB_BASE + 'main.js');
+        return new Response(await mainResponse.text(), {
           headers: { 'Content-Type': 'application/javascript;charset=UTF-8' }
         });
       
       case '/styles.css':
-        return new Response(stylesCs, {
+        const stylesResponse = await fetch(GITHUB_BASE + 'styles.css');
+        return new Response(await stylesResponse.text(), {
           headers: { 'Content-Type': 'text/css;charset=UTF-8' }
+        });
+      
+      case '/tests.js':
+        const testsJsResponse = await fetch(GITHUB_BASE + 'tests.js');
+        return new Response(await testsJsResponse.text(), {
+          headers: { 'Content-Type': 'application/javascript;charset=UTF-8' }
         });
       
       default:
